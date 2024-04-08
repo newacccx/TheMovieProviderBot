665b4d83836a832a2e68349b30a81c8862f0f778
newacccx <119074899+newacccx@users.noreply.github.com>
2024-04-09 00:50:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8
 CODE = (environ.get('CODE', 'https://telegra.ph/file/72f425007b22d28bd935e.jpg'))
 
 #stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
+STREAM_SITE = (environ.get('STREAM_SITE', 'shareus.io'))
 STREAM_API = (environ.get('STREAM_API', 'ae0832f39e24094a0661626a792e6a2b8880e310'))
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/adsbypass'))
 
